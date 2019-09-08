cdcf702140f1092329345d1e4c890c632d882abc
Nandor Dudas <nandor.dudas@gmail.com>
2019-09-08 18:37:57 +0200

    chore: update style

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
+import fabric from 'fabric'
 
-fabric.Canvas.prototype.historyUndo = []
-fabric.Canvas.prototype.historyRedo = []
+export default class Canvas extends fabric.Canvas {
+  constructor (el, options) {
+    super(el, options)
 
-fabric.Canvas.prototype.historyNext = function () {
-  return JSON.stringify(this.toDatalessJSON(this.extraProps));
-}
+    this._undoList = []
+    this._redoList = []
+    this._nextState = this._historyNext()
 
-fabric.Canvas.prototype.historyInit = function () {
-  this.historyUndo = [];
-  this.historyRedo = [];
-  this.historyNextState = this.historyNext();
-
-  this.on({
-    "object:added": this.historySaveAction,
-    "object:removed": this.historySaveAction,
-    "object:modified": this.historySaveAction
-  })
-
-}
-
-fabric.Canvas.prototype.historyDispose = function () {
-  this.off({
-    "object:added": this.historySaveAction,
-    "object:removed": this.historySaveAction,
-    "object:modified": this.historySaveAction
-  })
-}
-
-fabric.Canvas.prototype.historySaveAction = function () {
-  if (this.historyProcessing)
-    return;
-
-  const json = this.historyNextState;
-  this.historyUndo.push(json);
-  this.historyNextState = this.historyNext();
-}
-
-fabric.Canvas.prototype.undo = function () {
-  // The undo process will render the new states of the objects
-  // Therefore, object:added and object:modified events will triggered again
-  // To ignore those events, we are setting a flag.
-  this.historyProcessing = true;
-
-  const history = this.historyUndo.pop();
-  if (history) {
-    // Push the current state to the redo history
-    this.historyRedo.push(this.historyNext());
-
-    this.loadFromJSON(history).renderAll();
+    this.on({
+      'object:added': this._historySave,
+      'object:removed': this._historySave,
+      'object:modified': this._historySave
+    })
   }
 
-  this.historyProcessing = false;
-}
-
-fabric.Canvas.prototype.redo = function () {
-  // The undo process will render the new states of the objects
-  // Therefore, object:added and object:modified events will triggered again
-  // To ignore those events, we are setting a flag.
-  this.historyProcessing = true;
-  const history = this.historyRedo.pop();
-  if (history) {
-    // Every redo action is actually a new action to the undo history
-    this.historySaveAction();
-    
-    this.loadFromJSON(history).renderAll();
+  _historyNext () {
+    return JSON.stringify(this.toDatalessJSON(this.extraProps))
   }
 
-  this.historyProcessing = false;
+  _historyDispose () {
+    this.off({
+      'object:added': this._historySave,
+      'object:removed': this._historySave,
+      'object:modified': this._historySave
+    })
+  }
+
+  _historySave () {
+    if (this._isProcessing) {
+      return
+    }
+
+    const json = this._nextState
+
+    this._undoList.push(json)
+    this._nextState = this._historyNext()
+  }
+
+  undo () {
+    this._isProcessing = true
+
+    const history = this._undoList.pop()
+
+    if (history) {
+      this._redoList.push(this._historyNext())
+      this.loadFromJSON(history).renderAll()
+    }
+
+    this._isProcessing = false
+  }
+
+  redo () {
+    this._isProcessing = true
+
+    const history = this._redoList.pop()
+
+    if (history) {
+      this._historySave()
+      this.loadFromJSON(history).renderAll()
+    }
+
+    this._isProcessing = false
+  }
 }
