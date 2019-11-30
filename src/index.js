e68063b40069c78359bebafbafd7a908af0ea429
lyzerk <alm.ozdmr@gmail.com>
2019-11-30 19:21:54 +0300

    I have returned to vanillajs because the bundle of the webpack + babel is too high for simple plugin. New demo added.

--- src/index.js
+++ src/index.js
@@ -1,77 +1,86 @@
-import fabric from 'fabric'
 
-export default class Canvas extends fabric.Canvas {
-  get hasUndoItem () {
-    return this._undoList.length
+/**
+ * Override the initialize function for the _historyInit();
+ */
+fabric.Canvas.prototype.initialize = (function(originalFn) {
+  return function(...args) {
+    originalFn.call(this, ...args);
+    this._historyInit();
+    return this;
+  };
+})(fabric.Canvas.prototype.initialize);
+
+fabric.Canvas.prototype._historyNext = function () {
+  return JSON.stringify(this.toDatalessJSON(this.extraProps));
+}
+
+fabric.Canvas.prototype._historyInit = function () {
+  this.historyUndo = [];
+  this.historyRedo = [];
+  this.historyNextState = this._historyNext();
+
+  this.on({
+    "object:added": this._historySaveAction,
+    "object:removed": this._historySaveAction,
+    "object:modified": this._historySaveAction
+  });
+}
+
+fabric.Canvas.prototype._historyDispose = function () {
+  this.off({
+    "object:added": this._historySaveAction,
+    "object:removed": this._historySaveAction,
+    "object:modified": this._historySaveAction
+  })
+}
+
+fabric.Canvas.prototype._historySaveAction = function () {
+  if (this.historyProcessing)
+    return;
+
+  const json = this.historyNextState;
+  this.historyUndo.push(json);
+  this.historyNextState = this._historyNext();
+  this.fire('history:append', { json: json });
+}
+
+fabric.Canvas.prototype.undo = function () {
+  // The undo process will render the new states of the objects
+  // Therefore, object:added and object:modified events will triggered again
+  // To ignore those events, we are setting a flag.
+  this.historyProcessing = true;
+
+  const history = this.historyUndo.pop();
+  if (history) {
+    // Push the current state to the redo history
+    this.historyRedo.push(this._historyNext());
+
+    this.loadFromJSON(history).renderAll();
   }
 
-  get hasRedoItem () {
-    return this._redoList.length
+  this.historyProcessing = false;
+}
+
+fabric.Canvas.prototype.redo = function () {
+  // The undo process will render the new states of the objects
+  // Therefore, object:added and object:modified events will triggered again
+  // To ignore those events, we are setting a flag.
+  this.historyProcessing = true;
+  const history = this.historyRedo.pop();
+  if (history) {
+    // Every redo action is actually a new action to the undo history
+    this._historySaveAction();
+    
+    this.loadFromJSON(history).renderAll();
   }
 
-  constructor (el, options) {
-    super(el, options)
+  this.historyProcessing = false;
+}
 
-    this._undoList = []
-    this._redoList = []
-    this._nextState = this._historyNext()
-
-    this.on({
-      'object:added': this._saveHistory,
-      'object:removed': this._saveHistory,
-      'object:modified': this._saveHistory
-    })
-  }
-
-  _historyNext () {
-    return JSON.stringify(this.toDatalessJSON(this.extraProps))
-  }
-
-  
-  /**
-   * This method should be called on disposing the canvas
-   */
-  historyDispose () {
-    this.off({
-      'object:added': this._saveHistory,
-      'object:removed': this._saveHistory,
-      'object:modified': this._saveHistory
-    })
-  }
-
-  _saveHistory () {
-    if (this._isProcessing) {
-      return
-    }
-
-    this._undoList.push(this._nextState)
-
-    this._nextState = this._historyNext()
-  }
-
-  undo () {
-    this._isProcessing = true
-
-    const history = this._undoList.pop()
-
-    if (history) {
-      this._redoList.push(this._historyNext())
-      this.loadFromJSON(history).renderAll()
-    }
-
-    this._isProcessing = false
-  }
-
-  redo () {
-    this._isProcessing = true
-
-    const history = this._redoList.pop()
-
-    if (history) {
-      this._saveHistory()
-      this.loadFromJSON(history).renderAll()
-    }
-
-    this._isProcessing = false
-  }
+/**
+ * Clear undo and redo history stacks
+ */
+fabric.Canvas.prototype.clearHistory = function() {
+  this.historyUndo = [];
+  this.historyRedo = [];
 }
