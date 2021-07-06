4f12f2e0df07f2f4bbf0be8f2a38f52f8cf84490
scd7896 <scd1212@naver.com>
2021-07-06 21:55:25 +0900

    feat: to firstHistroy and history clear

--- src/index.js
+++ src/index.js
@@ -151,3 +151,15 @@ fabric.Canvas.prototype.onHistory = function() {
 
   this._historySaveAction();
 }
+
+/**
+ * to FirstHistory and ClearHistory
+ */
+fabric.Canvas.prototype.toResetHistory = function() {
+  let history = "{}";
+  while (this.historyUndo.length) history = this.historyUndo.pop();
+  this._loadHistory(history, "");
+  this.historyRedo = [];
+  this.historyUndo = [];
+  this.historyNextState = history;
+}
